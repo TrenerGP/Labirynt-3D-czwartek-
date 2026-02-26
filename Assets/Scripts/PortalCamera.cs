ca79eff3f1d1a1b6c7811463e46139d8a787e4dd
TrenerGP <kamieniarz.work@gmail.com>
2026-02-26 20:13:08 +0100

    lekcja24

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PortalCamera : MonoBehaviour
@@ -10,7 +11,30 @@ public class PortalCamera : MonoBehaviour
 
     void Update()
     {
+        FixedCamera();
+    }
 
+    private void FixedCamera()
+    {
+        Vector3 playerOffsetFromPortal = 
+            playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDiffrence =
+            Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationDifference =
+            Quaternion.AngleAxis(angularDiffrence, Vector3.up);
+        Vector3 newCameraDirection = 
+            portalRotationDifference * playerCamera.forward;
+
+        newCameraDirection = new Vector3(
+            newCameraDirection.x*-1,
+            newCameraDirection.y,
+            newCameraDirection.z*-1
+            );
+
+        transform.rotation =
+            Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 
 }
